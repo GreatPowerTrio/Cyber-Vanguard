dce444793de5305ea0701bafd56b51876f9dc6ac
Mikazuki Tomoko <142188839+Yesord@users.noreply.github.com>
2024-07-20 01:18:44 +0800

    Update __init__.py
    add img

--- raspberrypi_dev/img_detect/dev/utils/__init__.py
+++ raspberrypi_dev/img_detect/dev/utils/__init__.py
@@ -1,26 +1,28 @@
 from PIL import Image, ImageFont , ImageOps
 import numpy as np
 import os.path as osp
+import warnings
 
 package_dir = osp.dirname(osp.abspath(__file__))
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 def main():
-    print("*******************************************")
+    print("\n*******************************************")
     print("Initializing package")
-    print("*******************************************")
+    print("*******************************************\n")
     try:
-        print("*******************************************")
+        print("\n*******************************************")
         print("Check the num_detect.py")
-        print("*******************************************")
+        print("*******************************************\n")
         import num_detect 
         num_detect.main()
-        print("*******************************************")
+        print("\n*******************************************")
         print("Check the hardware.py")
-        print("*******************************************")
+        print("*******************************************\n")
         import hardware
         hardware.main()
     except Exception as e:
-        print("Error: ", e)
+        print(f"Error:{e}\n")
         ascill_art(osp.join(package_dir,"img/96763214_p7.png"))
         return
     ascill_art(osp.join(package_dir,"img/my_avater_small.jpg"))
@@ -35,10 +37,10 @@ def ascill_art(file):
     # 通过灰度覆盖这张图片 降低图片的亮度
     im = im.convert("L")
 
-    im = ImageOps.invert(im)
+    #im = ImageOps.invert(im)
 
     # 对图片进行降采样
-    sample_rate = 0.3
+    sample_rate = 0.4
 
     # 这段代码可以处理图片拉伸的情况
     font = ImageFont.truetype(osp.join(package_dir,"font/SourceCodePro-Black-1.ttf"))
@@ -57,7 +59,7 @@ def ascill_art(file):
     # symbols中定义了 我们字符画中的所有字符
     # 按照字符亮度升序排列
     # 文件转换的时候会不断的查询这个symbols字符集
-    symbols = np.array(list(" .-vM"))
+    symbols = np.array(list(" .-vM@#*"))
 
     # 设置这个字符集合的取值范围的最大和最小值[0,max_symbol_index)
     im = (im - im.min()) / (im.max() - im.min()) * (symbols.size - 1)
